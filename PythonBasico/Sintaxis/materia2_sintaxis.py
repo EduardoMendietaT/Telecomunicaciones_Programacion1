29c0fa8c34139b31b7edf0466566774201e05d28
Eduardo Mendieta <eduardo.mendieta.t@gmail.com>
2022-03-15 22:07:03 -0500

    hasta funciones!

---
+++ PythonBasico/Sintaxis/materia2_sintaxis.py
@@ -0,0 +1,5 @@
+# 10. PROCEDIMEINTOS: ------------------------------------------------------------
+def saludo_personalizado():
+    pass # permite que la funcion no tenga cuerpo y de esa manera no den errores.
+
+# tiempo: 06:46:14
