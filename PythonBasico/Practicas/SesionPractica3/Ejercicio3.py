4266d56f214e65ac88e734e8fa0e9f91d89c6d15
Eduardo Mendieta <eduardo.mendieta.t@gmail.com>
2022-04-18 23:33:25 -0500

    Hasta ejercicio 8 - Sesión práctica 3.

--- PythonBasico/Practicas/SesionPractica3/Ejercicio3.py
+++ PythonBasico/Practicas/SesionPractica3/Ejercicio3.py
@@ -8,13 +8,24 @@ a la serie de Fibonacci. Por ejemplo:
 si n = 10
 salida: 0 1 1 2 3 5 8 13 21 34
 '''
-n = int(input('\nIngrese un número mayor a 2: '))
-print(f'\n-------- los {n} dígitos correspondientes a la serie de Fibonacci --------\n')
-serie_fibonacci = '0 '
+# Números que inician la serie de Fibonacci.
+f1 = 0
+f2 = 1
 
+# Almacenaremos los n dijitos de la serie.
+serie_fibonacci = "0 1 "
+
+n = int(input("\nIngrese un número mayor a 2: "))
+
+# Inicialización de la variable contadora.
 cont = 0
-while cont <= n:
-    cont += 1
-    pass
 
-print(f'salida = {serie_fibonacci}')
+# La condición hasta n-2 porque ya tenemos los dos primeros dijitos almacenados.
+while cont < n - 2:
+    numero = f1 + f2 # Cada número de la serie es el resultado de sumar los 2 anteriores
+    serie_fibonacci += f"{numero} "
+    f1 = f2 # Actualizamos los números anteriores.
+    f2 = numero
+    cont += 1
+
+print(f"\nLa serie es: {serie_fibonacci}\n")
---
+++ PythonBasico/Practicas/SesionPractica3/Ejercicio9.py
@@ -0,0 +1,4 @@
+'''
+Escriba un programa que calcule la descomposición en factores primos de un número.
+Ejemplo para 500: Salida: 2 2 5 5 5
+'''
