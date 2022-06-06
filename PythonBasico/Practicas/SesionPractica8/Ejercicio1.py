f673c50faf0601690cbb66ba1f9056caf0ce4f95
Eduardo Mendieta <eduardo.mendieta.t@gmail.com>
2022-06-06 12:15:21 -0500

    preparando ejercicios para su resolucion.

---
+++ PythonBasico/Practicas/SesionPractica8/Ejercicio1.py
@@ -0,0 +1,9 @@
+'''
+Volver a la página https://projects.raspberrypi.org/en/projects/getting-started-with-the-sense-hat 
+y revisar los contenidos restantes del tutorial (desde sensing the environment hasta el final). 
+Luego, Generar un programa para jugar a "piedra, papel, o tijera". Para ello, el programa deberá 
+pedir al usuario una opción; esta será leída a través del joystick: izquierda-> piedra, arriva -> papel, 
+derecha -> tijera. Luego el programa seleccionará una opción aleatoriamente y verificar quien ganó para 
+ese round y lo anunciará en la pantalla. Adicionalmente, en la consola se deberá ir mostrando cuantas 
+veces el usuario a ganado vs el número de partidas.
+'''
